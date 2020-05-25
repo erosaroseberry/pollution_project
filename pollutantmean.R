98678646d4adfaccd6f12615de9a4cd2a527bce2
Evan Rosa <evanroseberry2@gmail.com>
2020-05-24 20:01:36 -0400

    Added descriptive comments. Included a loop to ensure that the 'pollutant' argument can only take the arguments 'nitrate' or 'sulfate'

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,28 +1,55 @@
+## E Rosa
+## May 24, 2020
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     
-    files <- list.files(directory)
-    column <- ifelse(pollutant == 'sulfate', 2, 3)
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of CSV files
     
-    pol_vals <- c()
-    for (i in id) {
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; may only be 'sulfate' or 'nitrate'
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return the mean of the pollutant accross all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    
+    
+    if ( xor(pollutant == 'sulfate', pollutant == 'nitrate') ) {
+    
+        files <- list.files(directory)
+        column <- ifelse(pollutant == 'sulfate', 2, 3)
+    
+        pol_vals <- c()
+        for (i in id) {
         
-        df <- read.csv(file = paste(directory, '/', files[i], sep = ''))
-        pol_data <- df[ , column][which(!is.na(df[ , column]))]
+            df <- read.csv(file = paste(directory, '/', files[i], sep = ''))
+            pol_data <- df[ , column][which(!is.na(df[ , column]))]
         
-        if (length(pol_data) > 0) {
+            if (length(pol_data) > 0) {
             
-            pol_vals[(length(pol_vals) + 1):(length(pol_vals) + length(pol_data))] <- pol_data
+                pol_vals[(length(pol_vals) + 1):(length(pol_vals) + length(pol_data))] <- pol_data
+        
+            }
+            else {
+            
+                next
+            
+            }
         
         }
-        else {
-            
-            next
-            
-        }
+    
+        return(mean(pol_vals))
         
     }
     
-    return(mean(pol_vals))
+    else {
+        
+        message("Error: pollutant must be a character vector of length 1 with value either 'nitrate' or 'sulfate'")
+        break
+        
+    }
     
 }
